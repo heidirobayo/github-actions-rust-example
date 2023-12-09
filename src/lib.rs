c37148f94a7afc234fd37d4cace054016b0a2c29
heidirobayo <150488054+heidirobayo@users.noreply.github.com>
2023-12-09 15:10:45 +0000

    ready for ci

---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+// craete a function two add two numbers
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
+// create a function to sub two numbers
+pub fn sub(a: i32) -> i32 {
+    a - 2
+}
+
+// create a function to multiply two numbers
+pub fn mul(a: i32) -> i32 {
+    a * 2
+}
+
+// create a function to divide two numbers
+pub fn div(a: i32) -> i32 {
+    a / 2
+}
